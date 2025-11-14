eac7d338df92e3ad819d5fb2f043b568f3bbc471
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-11-13 19:46:22 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import HeroSection from "@/components/HeroSection";
+import ApartmentsSection from "@/components/ApartmentsSection";
+import SkyDeckSection from "@/components/SkyDeckSection";
+import LocationSection from "@/components/LocationSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import ContactSection from "@/components/ContactSection";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <ApartmentsSection />
+      <SkyDeckSection />
+      <LocationSection />
+      <TestimonialsSection />
+      <ContactSection />
+    </>
+  );
+}
